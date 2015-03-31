d13ce18fc206bc844db10a2bf37eae21787a0692
Dametrious Peyton <dametrious.peyton@raritan.com>
2015-03-30 21:20:55 -0400

    added Chinese Taiwan localization

---
+++ translations/harbour-nemosyne-zh_TW.ts
@@ -0,0 +1,249 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_TW" version="2.1">
+<context>
+    <name>Answer</name>
+    <message>
+        <source>rating</source>
+        <translation>評分</translation>
+    </message>
+    <message>
+        <source>Rate</source>
+        <translation>評分</translation>
+    </message>
+</context>
+<context>
+    <name>CardDetail</name>
+    <message>
+        <source>Card Details</source>
+        <translation>字卡說明</translation>
+    </message>
+    <message>
+        <source>Card Type</source>
+        <translation>字卡類型</translation>
+    </message>
+    <message>
+        <source>Front To Back</source>
+        <translation>由前至後</translation>
+    </message>
+    <message>
+        <source>Front to Back, Back to Front</source>
+        <translation>由前至後再由後至前</translation>
+    </message>
+    <message>
+        <source>Question</source>
+        <translation>問題</translation>
+    </message>
+    <message>
+        <source>Answer</source>
+        <translation>答案</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>no database</source>
+        <translation>無資料庫</translation>
+    </message>
+    <message>
+        <source>No Cards</source>
+        <translation>無字卡</translation>
+    </message>
+</context>
+<context>
+    <name>Help</name>
+    <message>
+        <source>Help</source>
+        <translation>協助</translation>
+    </message>
+    <message>
+        <source>First, copy the mnemosyne.db file from your computer to this device. Then, you may study flash cards here. If you wish, copy the mnemosyne.db back to your computer to resume study there.</source>
+        <translation>首先從您的電腦中複製mnemosyne.db檔至裝置，而後您就可以開始字卡學習了。當然也可以將mnemosyne.db檔複製回電腦中複習。</translation>
+    </message>
+    <message>
+        <source>Importing Mnemosyne 2.x Database</source>
+        <translation>匯入 Mnemosyne 2.x 資料庫</translation>
+    </message>
+    <message>
+        <source>Starting a New Database</source>
+        <translation>建立一個新資料庫</translation>
+    </message>
+    <message>
+        <source>New databases will be created at the following path: </source>
+        <translation>可依循後述方式建立新資料庫:</translation>
+    </message>
+    <message>
+        <source>Score cards by providing a rating from 0 to 5. A rating of 0 indicates that you do not recall ever seeing the card. Whereas, a rating of 5 indicates complete recognition. Your score will determine how frequently a card re-appears in subsequent trainings.</source>
+        <translation>字卡評分由0至5，0代表您從未複習此字卡，而5代表完全了解。您的評分將會決定一張字卡於後續學習中的出現頻率。</translation>
+    </message>
+    <message>
+        <source>Rating</source>
+        <translation>評分</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>搜尋</translation>
+    </message>
+    <message>
+        <source>You may search through your card database by typing the search term in the tool bar. Matching cards will be displayed in a list view. Clicking an entry will bring up the question and answer texts.</source>
+        <translation>您可以於工具列輸入項目來搜尋您的字卡資料庫，符合的字卡將會以清單模式顯示，點擊後會顯示問題及答案。</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>Mnemosyne 2.x compatible</source>
+        <translation>可相容 Mnemosyne 2.x</translation>
+    </message>
+    <message>
+        <source>select database</source>
+        <translation>選取資料庫</translation>
+    </message>
+    <message>
+        <source>deselect</source>
+        <translation>取消選取</translation>
+    </message>
+    <message>
+        <source>select</source>
+        <translation>選取</translation>
+    </message>
+    <message>
+        <source>open existing database</source>
+        <translation>開啟現有資料庫</translation>
+    </message>
+    <message>
+        <source>nemosyne</source>
+        <translation>Nemosyne</translation>
+    </message>
+    <message>
+        <source>recently used</source>
+        <translation>目前使用</translation>
+    </message>
+    <message>
+        <source>database could not be opened</source>
+        <translation>無法開啟資料庫</translation>
+    </message>
+    <message>
+        <source>opening database</source>
+        <translation>開啟資料庫中</translation>
+    </message>
+    <message>
+        <source>database was not provided</source>
+        <translation>未提供資料庫</translation>
+    </message>
+    <message>
+        <source>First, copy the mnemosyne.db file from your computer to this device. Then, you may study flash cards here. If you wish, copy the mnemosyne.db back to your computer to resume study there.</source>
+        <translation>首先從您的電腦中複製mnemosyne.db檔至裝置，而後您就可以開始字卡學習了。當然也可以將mnemosyne.db檔複製回電腦中複習。</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>關於</translation>
+    </message>
+    <message>
+        <source>new db</source>
+        <translation>新資料庫</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>協助</translation>
+    </message>
+    <message>
+        <source>database could not be initialized</source>
+        <translation>無法初始化資料庫</translation>
+    </message>
+    <message>
+        <source>This action will delete the pre-existing database. Are you sure?</source>
+        <translation>如此將會刪除先前存在的資料庫，確定要這樣做嗎？</translation>
+    </message>
+    <message>
+        <source>Deleting old database</source>
+        <translation>刪除舊有資料庫</translation>
+    </message>
+    <message>
+        <source>A mobile flash card tool</source>
+        <translation>行動字卡學習工具</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>設定</translation>
+    </message>
+</context>
+<context>
+    <name>Question</name>
+    <message>
+        <source>No cards</source>
+        <translation>無字卡</translation>
+    </message>
+    <message>
+        <source>Add Card(s)</source>
+        <translation>新增字卡</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>編輯</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>刪除</translation>
+    </message>
+    <message>
+        <source>Add cards to begin</source>
+        <translation>於最初處新增字卡</translation>
+    </message>
+    <message>
+        <source>Deleting card</source>
+        <translation>刪除字卡中</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>設定</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>搜尋</translation>
+    </message>
+</context>
+<context>
+    <name>Search</name>
+    <message>
+        <source>Search</source>
+        <translation>搜尋</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>設定</translation>
+    </message>
+    <message>
+        <source>Font Size</source>
+        <translation>字型大小</translation>
+    </message>
+    <message>
+        <source>Rate cards by indicators instead of push-up menu</source>
+        <translation>由指引替代上拉選單來評分字卡</translation>
+    </message>
+    <message>
+        <source>Use Indicators</source>
+        <translation>使用指引</translation>
+    </message>
+    <message>
+        <source>Card Reset Time</source>
+        <translation>字卡重設時間</translation>
+    </message>
+</context>
+<context>
+    <name>StatusBar</name>
+    <message>
+        <source>scheduled</source>
+        <translation>定於</translation>
+    </message>
+    <message>
+        <source>unmemorized</source>
+        <translation>沒印象的</translation>
+    </message>
+    <message>
+        <source>active</source>
+        <translation>啟動</translation>
+    </message>
+</context>
+</TS>
