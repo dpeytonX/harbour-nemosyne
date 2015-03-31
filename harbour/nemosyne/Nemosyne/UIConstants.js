9254bc40810c028570767325623566f446c17f07
Dametrious Peyton <dametrious.peyton@raritan.com>
2015-03-30 21:23:59 -0400

    credit hanhsuan

--- harbour/nemosyne/Nemosyne/UIConstants.js
+++ harbour/nemosyne/Nemosyne/UIConstants.js
@@ -7,7 +7,8 @@ var appAuthors = ["Dametrious Peyton", "Thomas Amler - for AboutPage",
                   "jansol - for Rating Indicator Idea",
                   "ChMe - for Customizable Font Idea",
                   "TylerTemp - Chinese l10n (Transifex)",
-                  "Vistaus - Dutch l10n (Transifex)"]
+                  "Vistaus - Dutch l10n (Transifex)",
+                  "hanhsuan - Chinese (Taiwan) l10n (Transifex)"]
 var appCopyright = "Dametrious Peyton"
 var appLicense = ["GPLv3"]
 var appProjectInfo = ["https://github.com/prplmnky/harbour-nemosyne",
@@ -15,7 +16,7 @@ var appProjectInfo = ["https://github.com/prplmnky/harbour-nemosyne",
                       "https://github.com/prplmnky/sailfish-widgets",
                       "https://github.com/Armadill0/harbour-tasklist"]
 var appVersion = "1.5.0"
-var appYear = 2015
+var appYear = "2014-2015"
 
 var defaultDb = "nemosyne.db"
 var ratings = [5, 4, 3, 2, 1, 0]
