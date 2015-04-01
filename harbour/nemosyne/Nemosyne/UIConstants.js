21597f787bcabf1809ec2c0967db066db551a335
Dametrious Peyton <dametrious.peyton@raritan.com>
2015-04-01 15:02:58 -0400

    add credit jobe-m

--- harbour/nemosyne/Nemosyne/UIConstants.js
+++ harbour/nemosyne/Nemosyne/UIConstants.js
@@ -6,6 +6,7 @@ var appTitle = "Nemosyne"
 var appAuthors = ["Dametrious Peyton", "Thomas Amler - for AboutPage",
                   "jansol - for Rating Indicator Idea",
                   "ChMe - for Customizable Font Idea",
+                  "jobe-m - for QTranslator example",
                   "TylerTemp - Chinese l10n (Transifex)",
                   "Vistaus - Dutch l10n (Transifex)",
                   "hanhsuan - Chinese (Taiwan) l10n (Transifex)"]
@@ -15,7 +16,7 @@ var appProjectInfo = ["https://github.com/prplmnky/harbour-nemosyne",
                       "https://www.transifex.com/projects/p/harbour-nemosyne",
                       "https://github.com/prplmnky/sailfish-widgets",
                       "https://github.com/Armadill0/harbour-tasklist"]
-var appVersion = "1.5.0"
+var appVersion = "1.5.1"
 var appYear = "2014-2015"
 
 var defaultDb = "nemosyne.db"
